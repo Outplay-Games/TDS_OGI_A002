20ecf80308ef3b86510ba9a5fea40466af8f3144
Andrew <Andryha.p@gmail.com>
2019-03-03 12:23:36 +0200

    Basic movement, Character class and rotation
    Also a little prototype level and post processing.

---
+++ Assets/Character.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+	public float movementSpeed = 1f;
+
+	GameObject m_BodyObject;
+	Rigidbody m_Rigidbody;
+	Vector3 velocity = new Vector3(0f, 0f, 0f);
+	Vector3 m_EulerAngleVelocity;
+
+	void Start() {
+		// CHANGE THIS
+		m_BodyObject = transform.GetChild(0).gameObject;
+		// ^^
+		m_EulerAngleVelocity = new Vector3(0, 100, 0);
+		m_Rigidbody = GetComponent<Rigidbody>();
+	}
+
+	public void Move(float x, float z) {
+		if (m_Rigidbody) {
+			velocity.Set(x, 0f, z);
+			velocity = velocity.normalized * movementSpeed * Time.deltaTime;
+			m_Rigidbody.MovePosition(transform.position + velocity);
+		}
+	}
+
+	public void LookAt(Vector3 point) {
+		m_BodyObject.transform.LookAt(new Vector3(point.x, 0.5f, point.z));
+	}
+}
---
+++ Assets/Controller.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	public GameObject characterObject;
+	public Camera camera;
+	public LayerMask rayMask;
+
+	private Character m_character;
+	private Ray m_ray;
+	private RaycastHit hit;
+
+	// Start is called before the first frame update
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+		if (characterObject) {
+			if (!m_character) {
+				m_character = characterObject.GetComponent<Character>();
+			}
+
+			float x = Input.GetAxisRaw("Horizontal");
+			float z = Input.GetAxisRaw("Vertical");
+
+			if (x != 0f && z != 0f) {
+				m_character.Move(x, z);
+			} else if (x != 0f || z != 0f) {
+				m_character.Move(x, z);
+			}
+
+			m_ray = camera.ScreenPointToRay(Input.mousePosition);
+
+			if (Physics.Raycast(m_ray, out hit, Mathf.Infinity, rayMask)) {
+				if (hit.collider.CompareTag("Ground")) {
+					m_character.LookAt(hit.point);
+				}
+			}
+		}
+	}
+}
